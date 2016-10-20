327d4e4d38c6fa043d6262cf519cf7b34892dede
chrissbarr <spartanbarr@gmail.com>
2016-10-20 22:47:12 +1030

    New test sketch
    Combine both TinyFan and MightyFan test sketches into one file for tidyness / ease of use.

--- testSketch/testSketch.ino
+++ testSketch/testSketch.ino
@@ -1,15 +1,49 @@
-// TinyFan test sketch.
+/*
+ *  TinyFan / MightyFan test sketch
+ *  
+ *  Upload sketch to Arduino Mega and connect fan board. 
+ *  Supply fan board with power via screw terminals.
+ *  Each LED on the board should turn light up in sequence.
+ *  
+ */
+
+#define FAN_PIN_0   11    //tinyfan1    +   mightyfan1
+#define FAN_PIN_1   6     //tinyfan2    +   mightyfan2
+#define FAN_PIN_2   5     //                mightyfan3
+#define FAN_PIN_3   4     //                mightyfan4
+
+//uncomment MIGHTYFAN define to test MightyFan board
+//#define MIGHTYFAN
+
+#ifdef MIGHTYFAN
+  #define FAN_NUM 4
+#else
+  #define FAN_NUM 2
+#endif
+
+byte fanPins[] = {FAN_PIN_0, FAN_PIN_1, FAN_PIN_2, FAN_PIN_3};
 
 void setup() {
-  pinMode(6, OUTPUT);
-  pinMode(11, OUTPUT);
+
+  for(int i = 0; i < FAN_NUM; i++) {
+    pinMode(fanPins[i], OUTPUT);
+  }
+
 }
 
 void loop() {
- digitalWrite(11, HIGH);  
- digitalWrite(6, LOW); 
- delay(2000);               
- digitalWrite(11, LOW);
- digitalWrite(6, HIGH);   
- delay(2000);
+
+  for(int i = 0; i < FAN_NUM; i++) {
+
+    //set all pins to low
+    for(int j = 0; j < FAN_NUM; j++) {
+      digitalWrite(fanPins[j], LOW);
+    }
+
+    //set current pin high
+    digitalWrite(fanPins[i], HIGH);
+    
+    delay(1000);
+  }
+
 }
